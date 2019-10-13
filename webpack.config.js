b047113877930f02b5558c08698320e862ffecad
Piou-piou <pilloud.anthony@gmail.com>
2019-10-13 12:00:28 +0200

    [func] add title of nav [func] add file load for fonts [refa] variables scss file name

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,19 @@ module.exports = {
           'css-loader',
           'sass-loader',
         ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+              publicPath: '../fonts/'
+            }
+          }
+        ]
       }
     ]
   },

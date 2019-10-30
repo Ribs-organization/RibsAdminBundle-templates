c61b16e32eb7305ae0ab15a973afa4c08fdddeb6
Piou-piou <pilloud.anthony@gmail.com>
2019-10-30 08:22:19 +0100

    [del] api from index

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,6 @@
 import RibsApi from 'ribs-api';
 import RibsAjax from './RibsAjax';
 
-const api = new RibsApi('', 'cors');
 const ribsAjax = new RibsAjax('main', 'nav#left-nav');
 
 ribsAjax.loadPage('pages/dashboard.html');

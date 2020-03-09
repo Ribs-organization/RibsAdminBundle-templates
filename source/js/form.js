0e140dc0351d957c4d26dbfaf41e3290af169227
Piou-piou <pilloud.anthony@gmail.com>
2020-03-09 08:34:50 +0100

    [func] add autocomplete field and result

--- source/js/form.js
+++ source/js/form.js
@@ -6,7 +6,19 @@ const ribsForms = document.querySelectorAll('form.ribs-form');
 ribsForms.forEach((form) => {
   const autocompleteFields = form.querySelectorAll('.input-autocomplete');
 
-  autocompleteFields.forEach((autocompleteField) => {
+  const div = document.createElement('div');
+  div.classList.add('result-autocomplete');
+  document.querySelector('.input-autocomplete').after(div);
 
+  autocompleteFields.forEach((autocompleteField) => {
+    autocompleteField.addEventListener('keyup', (event) => {
+      const field = event.currentTarget;
+      if (field.value.length > 2) {
+        api.post(field.dataset.url, {autocomplete: field.value}, 'html').then((data) => {
+          const autocompleteResult = autocompleteField.parentNode.querySelector('.result-autocomplete');
+          autocompleteResult.innerHTML = data;
+        });
+      }
+    });
   });
 });
